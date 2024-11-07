595bab307b50377dd09dcab3f3f38d922232c1e7
doan <trancongdoan019@gmail.com>
2024-11-07 19:16:06 +0700

    update web responsive

--- HTML-CSS/TheBand/assets/js/main.js
+++ HTML-CSS/TheBand/assets/js/main.js
@@ -1,7 +1,11 @@
-const modal = document.querySelector('.js-modal')
-const btnClose = document.querySelector('.js-modal-close')
-const modalContainer = document.querySelector('.js-modal-container')
+const modal = document.querySelector('.js-modal');
+const btnClose = document.querySelector('.js-modal-close');
+const modalContainer = document.querySelector('.js-modal-container');
 const btnBuys = document.querySelectorAll('.js-buy-ticket');
+const header = document.getElementById('header');
+const mobileMenu = document.querySelector('.js-mobile-menu-btn')
+const currentHeight = header.clientHeight;
+const menuItems = document.querySelectorAll('.nav li a[href*="#"]')
 
 btnBuys.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -20,3 +24,25 @@ modal.addEventListener('click', () => {
 modalContainer.addEventListener('click', (event) => {
     event.stopPropagation()
 });
+
+mobileMenu.addEventListener('click', () => {
+    const isClose = header.clientHeight === currentHeight;
+
+    if (isClose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+})
+
+menuItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        const isParrentMenu = item.nextElementSibling && item.nextElementSibling.classList.contains('sub-nav');
+
+        if (isParrentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    })
+})
