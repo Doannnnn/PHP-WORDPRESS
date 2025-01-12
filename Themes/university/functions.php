4fbad550522912d837428f1015d43063c1129e23
doan <trancongdoan019@gmail.com>
2025-01-12 22:46:45 +0700

    new foder themes

---
+++ Themes/university/functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+function load_assets() {
+    wp_enqueue_style("font", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i", array(), "1.0", "all");
+    wp_enqueue_style("bootstrapcss", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", array(), "1.1", "all");
+    wp_enqueue_style("maincss", get_theme_file_uri("/build/index.css"), array(), "1.0.2", "all");
+    wp_enqueue_style("mainstylecss", get_theme_file_uri("/build/style-index.css"), array(), "1.0.3", "all");
+
+    wp_enqueue_script("script", get_theme_file_uri("/build/index.js"), array(), "1.02", true);
+}
+
+add_action("wp_enqueue_scripts", "load_assets");
+
+function add_menu() {
+    add_theme_support("menus");
+    register_nav_menus(array(
+        "FooterMenuOne" => "Footer Menu One",
+        "FooterMenuTwo" => "Footer Menu Two",
+    ));
+}
+
+add_action("init", "add_menu");
